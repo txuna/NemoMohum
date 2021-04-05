7b784dae26604e6fe817b539bc63fc8d7b7555a5
kissesy <39688948+kissesy@users.noreply.github.com>
2021-04-05 18:45:24 +0900

    set hit position

--- src/map/mountain/Mountain.gd
+++ src/map/mountain/Mountain.gd
@@ -3,6 +3,4 @@ extends "res://src/map/Map.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_map()
-	#load_map_data(0x8000)
 	set_enemy_signal()
-	#spawn_enemy()
