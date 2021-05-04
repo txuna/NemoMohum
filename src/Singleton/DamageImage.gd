3eaa0d414b3edd2a3b65e6a282f3f449b21a5a0b
kissesy <39688948+kissesy@users.noreply.github.com>
2021-05-05 00:35:17 +0900

    New Crit Number

--- src/Singleton/DamageImage.gd
+++ src/Singleton/DamageImage.gd
@@ -12,32 +12,19 @@ var no_crit_image = {
 	8 : load("res://assets/art/damage_skin/normal_damage/normal_8.png"),
 	9 : load("res://assets/art/damage_skin/normal_damage/normal_9.png"),
 }
-"""
-var no_crit_image = {
-	0 : load("res://assets/art/damage_skin/damage_number/NoRed0.0.png"),
-	1 : load("res://assets/art/damage_skin/damage_number/NoRed0.1.png"),
-	2 : load("res://assets/art/damage_skin/damage_number/NoRed0.2.png"),
-	3 : load("res://assets/art/damage_skin/damage_number/NoRed0.3.png"),
-	4 : load("res://assets/art/damage_skin/damage_number/NoRed0.4.png"),
-	5 : load("res://assets/art/damage_skin/damage_number/NoRed0.5.png"),
-	6 : load("res://assets/art/damage_skin/damage_number/NoRed0.6.png"),
-	7 : load("res://assets/art/damage_skin/damage_number/NoRed0.7.png"),
-	8 : load("res://assets/art/damage_skin/damage_number/NoRed0.8.png"),
-	9 : load("res://assets/art/damage_skin/damage_number/NoRed0.9.png"),
-}
-"""
+
 
 var crit_image = {
-	0 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.0.png"),
-	1 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.1.png"),
-	2 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.2.png"),
-	3 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.3.png"),
-	4 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.4.png"),
-	5 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.5.png"),
-	6 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.6.png"),
-	7 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.7.png"),
-	8 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.8.png"),
-	9 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.9.png"),
+	0 : load("res://assets/art/damage_skin/crit_damage/crit_0.png"),
+	1 : load("res://assets/art/damage_skin/crit_damage/crit_1.png"),
+	2 : load("res://assets/art/damage_skin/crit_damage/crit_2.png"),
+	3 : load("res://assets/art/damage_skin/crit_damage/crit_3.png"),
+	4 : load("res://assets/art/damage_skin/crit_damage/crit_4.png"),
+	5 : load("res://assets/art/damage_skin/crit_damage/crit_5.png"),
+	6 : load("res://assets/art/damage_skin/crit_damage/crit_6.png"),
+	7 : load("res://assets/art/damage_skin/crit_damage/crit_7.png"),
+	8 : load("res://assets/art/damage_skin/crit_damage/crit_8.png"),
+	9 : load("res://assets/art/damage_skin/crit_damage/crit_9.png"),
 }
 
 
