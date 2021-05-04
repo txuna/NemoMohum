11b21f74f8b109db1df6304320253746f0e890e9
kissesy <39688948+kissesy@users.noreply.github.com>
2021-05-04 23:44:16 +0900

    New Damage Skin

--- src/Singleton/DamageImage.gd
+++ src/Singleton/DamageImage.gd
@@ -1,6 +1,18 @@
 extends Node
 
-
+var no_crit_image = {
+	0 : load("res://assets/art/damage_skin/normal_damage/normal_0.png"),
+	1 : load("res://assets/art/damage_skin/normal_damage/normal_1.png"),
+	2 : load("res://assets/art/damage_skin/normal_damage/normal_2.png"),
+	3 : load("res://assets/art/damage_skin/normal_damage/normal_3.png"),
+	4 : load("res://assets/art/damage_skin/normal_damage/normal_4.png"),
+	5 : load("res://assets/art/damage_skin/normal_damage/normal_5.png"),
+	6 : load("res://assets/art/damage_skin/normal_damage/normal_6.png"),
+	7 : load("res://assets/art/damage_skin/normal_damage/normal_7.png"),
+	8 : load("res://assets/art/damage_skin/normal_damage/normal_8.png"),
+	9 : load("res://assets/art/damage_skin/normal_damage/normal_9.png"),
+}
+"""
 var no_crit_image = {
 	0 : load("res://assets/art/damage_skin/damage_number/NoRed0.0.png"),
 	1 : load("res://assets/art/damage_skin/damage_number/NoRed0.1.png"),
@@ -13,6 +25,7 @@ var no_crit_image = {
 	8 : load("res://assets/art/damage_skin/damage_number/NoRed0.8.png"),
 	9 : load("res://assets/art/damage_skin/damage_number/NoRed0.9.png"),
 }
+"""
 
 var crit_image = {
 	0 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.0.png"),
@@ -26,6 +39,7 @@ var crit_image = {
 	8 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.8.png"),
 	9 : load("res://assets/art/damage_skin/crit_damage_number/NoCri0.9.png"),
 }
+
 
 var get_hit_image = {
 	0 : load("res://assets/art/damage_skin/get_hit_damage_number/NoViolet0.0.png"),
