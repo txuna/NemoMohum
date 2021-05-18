b0c57c51daa20de7c416f875fff9bd729c7725cb
kissesy <39688948+kissesy@users.noreply.github.com>
2021-05-18 19:10:07 +0900

    Set Enemy State

--- src/Singleton/EnemyState.gd
+++ src/Singleton/EnemyState.gd
@@ -1,195 +1,76 @@
 class_name EnemyState
 
 var EnemyList = {
-	0xF003 : {
-		"enemy_code" : 0xF003,
-		"enemy_name" : "Frog",
+	0xF006 : {
+		"enemy_code" : 0xF006,
+		"enemy_name" : "기사단의 창병",
 		"state" : {
-			"attack" : 50,
-			"def" : 40, 
-			"max_hp" : 250,
-			"current_hp" : 250,
-			"speed" : 30,
-			"level" : 1,
-		},
-		"spoil" : {
-			"coin" : 10,
-			"exp" : 570,
-			"item" : [
-				{
-					"code" : 0xB000,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xB001,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xC000,
-					"numberof" : 1,
-					"percentage" : 100,
-				},
-				{
-					"code" : 0xC001,
-					"numberof" : 1,
-					"percentage" : 100,
-				}
-			]
-		}
-	},
-	0xF004 : {
-		"enemy_code" : 0xF004,
-		"enemy_name" : "Rabbit",
-		"state" : {
-			"attack" : 60,
-			"def" : 30, 
-			"max_hp" : 250,
-			"current_hp" : 250,
+			"attack" : 20,
+			"def" : 70, 
+			"max_hp" : 180,
+			"current_hp" : 180,
 			"speed" : 70,
 			"level" : 3,
 		},
 		"spoil" : {
 			"coin" : 15,
-			"exp" : 2000,
+			"exp" : 22,
 			"item" : [
-				{
-					"code" : 0xB000,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
 				{
 					"code" : 0xB001,
 					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xC002,
-					"numberof" : 1,
-					"percentage" : 100,
-				},
-			]
-		}
-	},
-	0xF005 : {
-		"enemy_code" : 0xF005,
-		"enemy_name" : "Squirrel",
-		"state" : {
-			"attack" : 20,
-			"def" : 70, 
-			"max_hp" : 250,
-			"current_hp" : 250,
-			"speed" : 90,
-			"level" : 5,
-		},
-		"spoil" : {
-			"coin" : 15,
-			"exp" : 2000,
-			"item" : [
-				{
-					"code" : 0xB000,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xB001,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xC003,
-					"numberof" : 1,
-					"percentage" : 100,
-				},
-			]
-		}
-	},
-	0xF006 : {
-		"enemy_code" : 0xF006,
-		"enemy_name" : "Spearman",
-		"state" : {
-			"attack" : 20,
-			"def" : 70, 
-			"max_hp" : 250,
-			"current_hp" : 250,
-			"speed" : 90,
-			"level" : 5,
-		},
-		"spoil" : {
-			"coin" : 15,
-			"exp" : 2000,
-			"item" : [
-				{
-					"code" : 0xB000,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xB001,
-					"numberof" : 1,
-					"percentage" : 100, 
+					"percentage" : 30, 
 				},
 			]
 		}
 	},
 	0xF007 : {
 		"enemy_code" : 0xF007,
-		"enemy_name" : "Swordman",
+		"enemy_name" : "기사단의 검병",
 		"state" : {
-			"attack" : 50,
-			"def" : 100, 
-			"max_hp" : 500,
-			"current_hp" : 500,
-			"speed" : 130,
-			"level" : 5,
+			"attack" : 10,
+			"def" : 70, 
+			"max_hp" : 100,
+			"current_hp" : 100,
+			"speed" : 70,
+			"level" : 1,
 		},
 		"spoil" : {
 			"coin" : 15,
-			"exp" : 2000,
+			"exp" : 10,
 			"item" : [
 				{
 					"code" : 0xB000,
 					"numberof" : 1,
-					"percentage" : 100, 
+					"percentage" : 40, 
 				},
-				{
-					"code" : 0xB001,
-					"numberof" : 1,
-					"percentage" : 100, 
-				},
-				{
-					"code" : 0xC003,
-					"numberof" : 1, 
-					"percentage" : 100,
-				}
 			]
 		}
 	},
 	0xF008 : {
 		"enemy_code" : 0xF008,
-		"enemy_name" : "Sentryman",
+		"enemy_name" : "기사단의 경비병",
 		"state" : {
-			"attack" : 70,
-			"def" : 20, 
-			"max_hp" : 250,
-			"current_hp" : 250,
-			"speed" : 130,
-			"level" : 5,
+			"attack" : 38,
+			"def" : 120, 
+			"max_hp" : 300,
+			"current_hp" : 300,
+			"speed" : 80,
+			"level" : 6,
 		},
 		"spoil" : {
 			"coin" : 15,
-			"exp" : 2000,
+			"exp" : 35,
 			"item" : [
 				{
 					"code" : 0xB000,
 					"numberof" : 1,
-					"percentage" : 100, 
+					"percentage" : 30, 
 				},
 				{
-					"code" : 0xB001,
+					"code" : 0xB003,
 					"numberof" : 1,
-					"percentage" : 100, 
+					"percentage" : 20, 
 				},
 			]
 		}
@@ -218,6 +99,80 @@ var EnemyList = {
 					"code" : 0xB001,
 					"numberof" : 1,
 					"percentage" : 100, 
+				},
+			]
+		}
+	},
+	0xF00A : {
+		"enemy_code" : 0xF00A,
+		"enemy_name" : "기사단의 맨손병",
+		"state" : {
+			"attack" : 120,
+			"def" : 70, 
+			"max_hp" : 130,
+			"current_hp" : 130,
+			"speed" : 35,
+			"level" : 2,
+		},
+		"spoil" : {
+			"coin" : 15,
+			"exp" : 13,
+			"item" : [
+				{
+					"code" : 0xB000,
+					"numberof" : 1,
+					"percentage" : 20, 
+				},
+			]
+		}
+	},
+	0xF00B : {
+		"enemy_code" : 0xF00B,
+		"enemy_name" : "기사단의 방패병",
+		"state" : {
+			"attack" : 22,
+			"def" : 400, 
+			"max_hp" : 1000,
+			"current_hp" : 1000,
+			"speed" : 30,
+			"level" : 13,
+		},
+		"spoil" : {
+			"coin" : 15,
+			"exp" : 62,
+			"item" : [
+				{
+					"code" : 0xB003,
+					"numberof" : 1,
+					"percentage" : 30, 
+				},
+				{
+					"code" : 0xB002,
+					"numberof" : 1,
+					"percentage" : 20, 
+				},
+			]
+		}
+	},
+	0xF00C : {
+		"enemy_code" : 0xF00C,
+		"enemy_name" : "기사단의 궁병",
+		"state" : {
+			"attack" : 45,
+			"def" : 150, 
+			"max_hp" : 500,
+			"current_hp" : 500,
+			"speed" : 75,
+			"level" : 10,
+		},
+		"spoil" : {
+			"coin" : 15,
+			"exp" : 45,
+			"item" : [
+				{
+					"code" : 0xB002,
+					"numberof" : 1,
+					"percentage" : 15, 
 				},
 			]
 		}
